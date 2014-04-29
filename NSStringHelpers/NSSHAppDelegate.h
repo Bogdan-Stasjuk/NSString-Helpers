5f916263d7ff73b102bcfd487afbdcf97e8ad25a
Bogdan Stasjuk <Bogdan.Stasjuk@gmail.com>
2014-04-29 22:54:07 +0300

    Initial Commit

---
+++ NSStringHelpers/NSSHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NSSHAppDelegate.h
+//  NSStringHelpers
+//
+//  Created by Bogdan Stasjuk on 4/29/14.
+//  Copyright (c) 2014 Bogdan Stasjuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSSHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSStringHelpers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NSStringHelpers
+//
+//  Created by Bogdan Stasjuk on 4/29/14.
+//  Copyright (c) 2014 Bogdan Stasjuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NSSHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NSSHAppDelegate class]));
+    }
+}
